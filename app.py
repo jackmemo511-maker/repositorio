8fd5c1fa79261832de6c00d44c678175f2647463
jackmemo511-maker <jackmemo511@gmail.com>
2026-07-15 21:23:38 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+car_data = pd.read_csv(r'C:\Users\Axionixsed\Downloads\vehicles_us.csv')
+
+
+hist_button = st.button('Construir histograma')
+
+# Lógica a ejecutar cuando se hace clic en el botón
+if hist_button:
+    # Escribir un mensaje en la aplicación
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un histograma utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de histograma
+    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Distribución del Odómetro')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
+ 
+import streamlit as st
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')

8aa21cc825db4749105253d656e159a5725b8a39
jackmemo511-maker <jackmemo511@gmail.com>
2026-07-15 21:49:41 -0600

    Refactor histogram creation logic in app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.header('Datos De Autos')
+
 
 car_data = pd.read_csv('vehicles_us.csv')
 
-
+st.header('Datos De Autos')
 hist_button = st.button('Construir histograma')
 
 # Lógica a ejecutar cuando se hace clic en el botón
